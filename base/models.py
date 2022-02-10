8e8e9f0e51cb4a60318cbc037483c3ec6640505e
Nishant Sethi <sethi.nishant43@gmail.com>
2022-02-10 17:01:41 +0530

    changes

--- base/models.py
+++ base/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Urls(models.Model):
-    name = models.CharField(max_length=100, blank=True, null= True)
     long_url = models.URLField(null=False, blank=False)
     short_url = models.CharField(max_length=500, null=False, blank=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
